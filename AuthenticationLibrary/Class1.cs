fcc0a722a45429590c905190f5c3267558613197
Rathieshprasad <rathiesh2002@gmail.como>
2023-05-31 00:13:11 +0530

    adding files

---
+++ AuthenticationLibrary/Class1.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+
+namespace AuthenticationLibrary
+{
+    public class AzureADAuthenticator
+    {
+        private static string clientId;
+        private static string clientSecret;
+        private static string authority = "https://login.microsoftonline.com/common";
+
+        public static string GetAuthorizationUrl(string scope, string redirectUri, string clientId, string clientSecret)
+        {
+            AzureADAuthenticator.clientId = clientId;
+            AzureADAuthenticator.clientSecret = clientSecret;
+
+            var authContext = new AuthenticationContext(authority);
+            var authUri = authContext.GetAuthorizationRequestUrlAsync(
+                scope,
+                clientId,
+                new Uri(redirectUri),
+                UserIdentifier.AnyUser,
+                null
+            ).Result;
+
+            return authUri.ToString();
+        }
+
+        public static string ProcessAuthorizationCode(string code, string scope, string redirectUri, string clientId, string clientSecret)
+        {
+            AzureADAuthenticator.clientId = clientId;
+            AzureADAuthenticator.clientSecret = clientSecret;
+
+            var authContext = new AuthenticationContext(authority);
+            var result = authContext.AcquireTokenByAuthorizationCodeAsync(
+                code,
+                new Uri(redirectUri),
+                new ClientCredential(clientId, clientSecret),
+                scope
+            ).Result;
+            Console.WriteLine(result);
+            return result.AccessToken;
+        }
+    }
+}
+
---
+++ webTest2/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using AuthenticationLibrary;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace webTest2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            var scope = "https://graph.microsoft.com/user.read";
+            var redirectUri = Request.Scheme + "://" + Request.Host + "/Home/ProcessToken";
+            var clientId = _configuration["AzureAd:ClientId"];
+            var clientSecret = _configuration["AzureAd:ClientSecret"];
+
+            var authUrl = AzureADAuthenticator.GetAuthorizationUrl(scope, redirectUri, clientId, clientSecret);
+            Console.WriteLine(authUrl);
+
+            return Redirect(authUrl);
+        }
+
+        public ActionResult Success()
+        {
+            return View();
+        }
+
+        public ActionResult ProcessToken(string code, string error=null)
+        {
+            Console.WriteLine("Authentication code");
+            if (error != null)
+            {
+                // Handle the error
+                Console.WriteLine($"Authentication error: {error}");
+                return RedirectToAction("Index");
+            }
+            Console.WriteLine($"Authentication code: {code}");
+            var scope = "https://graph.microsoft.com";
+            var redirectUri = "https://localhost:7056/Home/ProcessToken";
+            var clientId = _configuration["AzureAd:ClientId"];
+            var clientSecret = _configuration["AzureAd:ClientSecret"];
+
+            var accessToken = AzureADAuthenticator.ProcessAuthorizationCode(code, scope, redirectUri, clientId, clientSecret);
+
+            // Use the access token in 'accessToken' to call the Microsoft Graph API
+
+            return RedirectToAction("Success");
+        }
+    }
+}
